d5956c9d569c6a8fc111c4e784247be0c6a0eb7d
PartyCrashSquad <partycrashsquad@gmail.com>
2025-10-16 19:16:18 -0500

    Add files via upload

---
+++ Quadnode.js
@@ -0,0 +1,17 @@
+
+
+
+class Quadnode{
+    constructor(x, y, bounds){
+        this.x = x
+        this.y = y
+        this.bounds = bounds
+        this.members = []
+        this.children = []
+        this.limit = 3
+    }
+
+    checkMembers(arr){
+
+    }
+}
